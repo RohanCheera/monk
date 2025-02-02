6472fbd1699862f0d26dda46661423677285abb1
Rohan Cheera <113199793+RohanCheera@users.noreply.github.com>
2025-02-02 08:05:13 +0530

    Update combination sum.cpp
    for loop method of back tracking is updated to the existig solution page.

--- backtracking/combination sum.cpp
+++ backtracking/combination sum.cpp
@@ -1,4 +1,5 @@
 // Leetcode Qn Link : https://leetcode.com/problems/combination-sum
+
 //Method 1: Without using for loop
 class Solution {
 public:
@@ -22,6 +23,37 @@ public:
         vector<vector<int>> ans;
         vector<int> temp;
         solve(0,candidates,target,temp,ans);
+        return ans;
+    }
+};
+
+
+// Method 2: using for loop  and sorting 
+
+class Solution {
+public:
+    void solve(int idx, vector<int>& candidates, int target, vector<int> temp,
+               vector<vector<int>>& ans) {
+
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); i++) {
+
+            if (candidates[i] > target)
+                break;
+            temp.push_back(candidates[i]);
+            solve(i, candidates, target - candidates[i], temp, ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0, candidates, target, temp, ans);
         return ans;
     }
 };
