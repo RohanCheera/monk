69f54f8b7e54c605a99875dac55bf357f80b95e8
Rohan Cheera <113199793+RohanCheera@users.noreply.github.com>
2025-02-02 08:39:23 +0530

    Update combination sum.cpp
    method 3 is added to the existing combination sum codes

--- backtracking/combination sum.cpp
+++ backtracking/combination sum.cpp
@@ -57,3 +57,32 @@ public:
         return ans;
     }
 };
+
+// Method 3: for loop without sorting 
+class Solution {
+public:
+    void solve(int idx, vector<int>& candidates, int target, vector<int> temp,
+               vector<vector<int>>& ans) {
+        if (target < 0)
+            return;
+
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = idx; i < candidates.size(); i++) {
+
+            temp.push_back(candidates[i]);
+            solve(i, candidates, target - candidates[i], temp, ans);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        vector<vector<int>> ans;
+        vector<int> temp;
+        solve(0, candidates, target, temp, ans);
+        return ans;
+    }
+};
